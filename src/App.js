3de3a70173e6caa733f3288ff6a870ea2db3e10c
Aingkaran <aingkaran1995@gmail.com>
2022-10-09 17:51:40 -0400

    Complete Random Function and IMG Function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,68 @@ import './styles/App.css';
 import React, { useState, useEffect } from "react";
 import Pokemon from './components/Pokemon'
 
+import charizard from './Images/01.png';
+import blastoise from './Images/02.png';
+import venasaur from './Images/03.png';
+import greninja from './Images/04.png';
+import dragonite from './Images/05.png';
+import pikachu from './Images/06.png';
+import sceptile from './Images/07.png';
+import infernape from './Images/08.png';
+import mewtwo from './Images/09.png';
+import golem from './Images/10.png';
+import machomp from './Images/11.png';
+import alakazam from './Images/12.png';
+
 const App=()=> {
-  
+  const [pokemonArray, setpokemonArray]=useState([charizard,blastoise,venasaur,greninja,dragonite,pikachu,sceptile,infernape,mewtwo,golem,machomp,alakazam])
+  const [counter, setCounter] = useState(0);
+
+
+  const pokemonRandom=()=>{
+      let pokemonsubArray= [...pokemonArray]
+      let index= pokemonsubArray.length
+      
+
+      while (index != 0){
+          let random_num = Math.floor(Math.random() * index)
+          index--;
+          [pokemonsubArray[index], pokemonsubArray[random_num]] = [pokemonsubArray[random_num], pokemonsubArray[index]];
+
+      }
+
+      setpokemonArray(pokemonsubArray)
+      }
+
+  // useEffect(() => {
+  //     const nextPokemon = () => {
+  //         if (counter==0){
+  //         setCounter(1)
+  //         }
+  //         else{
+  //         setCounter(0)
+  //         }
+  //     };
+  //     const btn=document.querySelectorAll(".images")
+  //     btn.forEach((button)=>{
+  //         button.addEventListener("click", nextPokemon);
+  //         console.log(button)
+  //     })
+
+  //     return () => {
+  //         btn.forEach((button)=>{
+  //             button.removeEventListener("click", nextPokemon);
+  //         })        };
+  //     },[counter]);
+
+
 
 
   return (
     <div className="App">
       <div>
         </div>
-        <Pokemon></Pokemon>
+        <Pokemon randomFunction={pokemonRandom} Array={pokemonArray}></Pokemon>
     </div>
   );
 }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Pokemon.css"
 
+
+
+import uniqid from "uniqid";
 import charizard from '../Images/01.png';
 import blastoise from '../Images/02.png';
 import venasaur from '../Images/03.png';
@@ -14,56 +17,17 @@ import golem from '../Images/10.png';
 import machomp from '../Images/11.png';
 import alakazam from '../Images/12.png';
 
-import uniqid from "uniqid";
+const Pokemon=(props)=>{
+    const { Array } = props;
+    const { randomFunction } = props;
 
-
-const Pokemon=()=>{
-    const [pokemonArray, setpokemonArray]=useState([charizard,blastoise,venasaur,greninja,dragonite,pikachu,sceptile,infernape,mewtwo,golem,machomp,alakazam])
-    const [counter, setCounter] = useState(0);
-
-
-    const pokemonRandom=()=>{
-        let pokemonsubArray= pokemonArray
-        let index= pokemonsubArray.length
-        while (index != 0){
-            let random_num = Math.floor(Math.random() * index)
-            index--;
-            [pokemonsubArray[index], pokemonsubArray[random_num]] = [pokemonsubArray[random_num], pokemonsubArray[index]];
-            setpokemonArray(pokemonsubArray)
-
-        }
-
-        }
-
-    useEffect(() => {
-        const nextPokemon = () => {
-            if (counter==0){
-            setCounter(1)
-            }
-            else{
-            setCounter(0)
-            }
-        };
-        const btn=document.querySelectorAll(".images")
-        btn.forEach((button)=>{
-            button.addEventListener("click", nextPokemon);
-        })
-    
-        return () => {
-            btn.forEach((button)=>{
-                button.removeEventListener("click", nextPokemon);
-            })        };
-        },[counter]);
-            
-          
 
     return(
         <div>   
-            <button className="btn" onClick={pokemonRandom}>CLICK ME</button>
 
             <div className="pokemonCards">
-                {pokemonArray.map((pokemon) => (
-                    <img className="images" key={uniqid()} alt={pokemon} src={pokemon}></img>
+                {Array.map((pokemon) => (
+                    <img className="images" key={uniqid()} alt={pokemon} src={pokemon} onClick={randomFunction}></img>
 
                     ))}
             </div>
